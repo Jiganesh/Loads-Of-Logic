e5f27ab120013709256ff5228df9f6c5d9f56ebc
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2021-11-24 21:36:07 +0530

    solved singleElementInASortedArray

--- binarySearch/singleElementInASortedArray.java
+++ binarySearch/singleElementInASortedArray.java
@@ -1,6 +1,6 @@
-package concepts.binarySearch;
+package binarySearch;
 
-public class SingleElementInASortedArray {
+public class singleElementInASortedArray {
     public static void main(String[] args) {
         SolutionSEIASA solution = new SolutionSEIASA();
         System.out.println(solution.singleNonDuplicate(new int[] {1,1,2,3,3,4,4,8,8}));
@@ -19,7 +19,7 @@ class SolutionSEIASA {
            int mid = start + (end -start)/2;
 
            if(array[mid]== array[mid+1]) mid = mid-1 ;
-           if((mid -start+1 )%2 !=0) end = mid;
+           if((mid -start+1 )%2 != 0) end = mid;
            else start = mid+1;
        }
        return array[start];   
