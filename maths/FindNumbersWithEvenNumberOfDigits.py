0e263de0d49fcd0f4ff7acdab03a33db0ad32889
varun21vaidya <varun21vaidya@gmail.com>
2021-12-02 18:43:44 +0530

    Added stack problems

---
+++ maths/FindNumbersWithEvenNumberOfDigits.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        
+        #1 with string and len
+        count=0
+        for i in range(0,len(nums)):
+            if (len(str(nums[i]))%2==0): count+=1
+        return count
+    
+        '''
+        #2 Using division
+        count=0
+        for i in nums:
+            cur=0
+            while(i>0):
+                cur+=1
+                i=i//10
+            if cur%2==0: count+=1
+        return count       
+       
+        #3 using log and floor 
+        #floor(log10(num)) gives 1 value less than length of integer ie odd
+        return sum((floor(log10(i)))%2 for i in nums)
+        '''
