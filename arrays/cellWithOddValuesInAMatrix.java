9929a0e883b65a5b964614fad78c0656baf38357
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2021-12-21 22:23:40 +0530

    easy problem cells with odd values with 4 solutions solved

--- arrays/cellWithOddValuesInAMatrix.java
+++ arrays/cellWithOddValuesInAMatrix.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.BitSet;
+
 public class cellWithOddValuesInAMatrix {
     public static void main(String[] args) {
         int m =2;
@@ -11,8 +13,19 @@ public class cellWithOddValuesInAMatrix {
         int m1 =2;
         int n1= 2;
         int [][] indices1 = new int[][] {{1,1},{0,0}};
-        SolutionCWOVIAM solution1 = new SolutionCWOVIAM() ; 
-        System.out.println(solution1.oddCells(m1,n1,indices1));
+        
+        System.out.println(solution.oddCells(m,n,indices));
+        System.out.println(solution.oddCellsApproach2(m,n,indices));
+        System.out.println(solution.oddCellsApproach3(m,n,indices));
+        System.out.println(solution.oddCellsApproach4(m,n,indices));
+
+
+        System.out.println(solution.oddCells(m1,n1,indices1));
+        System.out.println(solution.oddCellsApproach2(m1,n1,indices1));
+        System.out.println(solution.oddCellsApproach3(m1,n1,indices1));
+        System.out.println(solution.oddCellsApproach4(m1,n1,indices1));
+
+
     }
     
 }
@@ -44,5 +57,73 @@ class SolutionCWOVIAM {
             //System.out.println();
         }
         return count;
+    }
+
+    //TC : O(m*n)+O(L)
+    //SC : O(m+n)
+
+    public int oddCellsApproach2(int m , int n , int[][] indices){
+
+        boolean[] rows = new boolean[m];
+        boolean[] columns = new boolean[n];
+        int count=0;
+        for (int i=0; i< indices.length; i++){
+            rows[indices[i][0]] ^= true;
+            columns[indices[i][1]] ^= true;
+        }
+
+        for (int i =0; i<rows.length; i++){
+            for(int j = 0 ; j< columns.length; j++){
+                if (rows[i]^ columns[j]) count++;
+            }
+        }
+        return count;
+
+        // int r = countTrue(rows);
+        // int c = countTrue(columns);
+        
+        // return r*n+c*m-2*r*c;
+    }
+
+    
+    //TC : O(L+m+n)
+    //SC : O(m+n)
+    public int oddCellsApproach3(int m , int n , int[][] indices){
+
+        boolean[] rows = new boolean[m];
+        boolean[] columns = new boolean[n];
+
+        for (int i=0; i< indices.length; i++){
+            rows[indices[i][0]] ^= true;
+            columns[indices[i][1]] ^= true;
+        }
+
+
+        int r = countTrue(rows);
+        int c = countTrue(columns);
+        
+        return r*n+c*m-2*r*c;
+    }
+
+    public int countTrue(boolean[] array){
+        int result =0;
+        for (int i =0; i<array.length; i++){
+            if (array[i]) result++;
+        }
+        return result;
+    }
+
+    //TC : O(L)
+    //SC : O(m+n)
+    public int oddCellsApproach4(int m, int n, int[][] indices) {
+        BitSet oddRows = new BitSet(m), oddCols = new BitSet(n);
+        int cntRow = 0, cntCol = 0;
+        for (int[] idx : indices) {
+            oddRows.flip(idx[0]);
+            oddCols.flip(idx[1]);
+            cntRow += oddRows.get(idx[0]) ? 1 : -1;
+            cntCol += oddCols.get(idx[1]) ? 1 : -1;
+        }
+        return (n - cntCol) * cntRow + (m - cntRow) * cntCol;   
     }
 }
