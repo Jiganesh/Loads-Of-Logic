0ccac2e34028c4f1c17c9e1a1dee54504f93f93b
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2021-12-23 01:21:45 +0530

    resolved link issue

--- #dev/allProblemsSolved.py
+++ #dev/allProblemsSolved.py
@@ -12,7 +12,7 @@ writeReadme.write(title)
 writeReadme.write("<ol>\n")
 for i in lines:
     p=i.index("(")
-    writeReadme.write("<li><a href ='"+ i + "'>"+i[:p]+"</a>"+i[p:]+"</li>\n")
+    writeReadme.write("<li><a href ='"+ i[:i.rindex("/")] + "'>"+i[:p]+"</a>"+i[p:]+"</li>\n")
 'Additional Problems Here'
 writeReadme.write(
     '<li><a href = "https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118523/offering/1380947?leftPanelTab=0">https://www.codingninjas.com/reverse-first-k-elements-of-queue</a>(Easy)<li> '
