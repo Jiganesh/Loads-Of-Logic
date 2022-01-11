33382559fae39c87aea9fa56615d7a6d597745d9
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2022-01-11 22:16:08 +0530

    solved Number of Descent

---
+++ dynamicProgramming/numberOfSmoothDescentPeriodsOfAStock.java
@@ -0,0 +1,43 @@
+package dynamicProgramming;
+
+// https://leetcode.com/problems/number-of-smooth-descent-periods-of-a-stock/
+
+public class numberOfSmoothDescentPeriodsOfAStock{
+    public static void main(String[] args) {
+        
+    }
+}
+
+class SolutionNOSDPOAS{
+    public long getDescentPeriods(int[] prices) {
+        long start=0, val=1;
+        for (int i =1 ; i< prices.length; i++){
+            if(prices[i-1]-prices[i]==1){
+                val += i-start+1;
+            }else{
+                start=i;
+                val+=1;
+            }
+        }
+        return val;        
+    }
+    
+    /* Example
+
+
+    prices = [3,2,1,4]
+
+    0       1           2           3
+    
+    3       2           1           4
+    [3]     [2]         [1]         [4]
+            [3,2]       [2,1]
+                        [3,2,1]
+
+    nextCount = previous + end - start + 1
+    
+    
+    */
+
+    
+}
