75d3c92f05d652efdd939e82957e83c13e75a1d8
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2022-01-13 03:01:59 +0530

    few README Changes and corrected new pull request

--- .dev/guidelines.md
+++ .dev/guidelines.md
@@ -5,6 +5,8 @@
 - fileName should be same name as the problemName
 - Contribute in any Language
 - Update ReadMe file simulateneously
-- try to explain your approach as much as possible
+- try to explain your approach as much as possible (IMPORTANT)
+- Use code Formatter and check if everything is properly aligned
+- Do not leave many empty lines
 
 If prefix start with leetcode it means the solutions was for Interactive Leetcode problem
--- trees/insertIntoABinarySearchTree.java
+++ trees/insertIntoABinarySearchTree.java
@@ -14,36 +14,67 @@ class TreeNode {
 }
 
 class Solution {
+
+
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        
-       TreeNode NewNode = new TreeNode(val);
-         TreeNode dummy = root;
-        
-        if(root == null)
-            return NewNode;
-            
-        
-         while(true)
-         {
-             if(dummy.val > val && dummy.left == null){
-                   dummy.left = NewNode;
-                 break;
-             }
-             if(dummy.val < val && dummy.right == null){
-                   dummy.right = NewNode;
-                 break;
-             }
-             
-             if(dummy.val > val && dummy.left != null){
-                  dummy = dummy.left;
-                 continue;
-             }
-             if(dummy.val < val && dummy.right != null){
-                 dummy = dummy.right;
-                 continue;
-             }    
-         }
-        
-        
+        TreeNode newNode = new TreeNode(val);
+        if (root== null) return newNode;
+
+        TreeNode currentNode = root;
+        while(currentNode!=null){
+
+            if(currentNode.val> val){
+                if (currentNode.left !=null){
+                    currentNode = currentNode.left;
+                }else{
+                    currentNode.left= newNode;
+                    break;
+                }
+            }else{
+                if (currentNode.right !=null){
+                    currentNode = currentNode.right;
+                }else{
+                    currentNode.right= newNode;
+                    break;
+                }
+            }
+        }
         return root;
- }}
+    }
+
+    // Efficient  (no nested conditions)
+    public TreeNode insertIntoBSTApproach2(TreeNode root, int val) {
+
+        // Making newNode for val
+        // initializing root to currentNode to traverse
+        TreeNode newNode = new TreeNode(val);
+        TreeNode currentNode = root;
+
+        //If root is null obviously we return newNode as it will be only node
+        if (root == null) return newNode;
+
+        
+        while (true) {
+            // we move if the val is greater to left or right if it is less and if the next node is not null
+            if (currentNode.val > val && currentNode.left != null) {
+                currentNode = currentNode.left;
+                continue;
+            }
+            if (currentNode.val < val && currentNode.right != null) {
+                currentNode = currentNode.right;
+                continue;
+            }
+
+            // if next node is null based on the condition we insert to new node
+            if (currentNode.val > val && currentNode.left == null) {
+                currentNode.left = newNode;
+                break;
+            }
+            if (currentNode.val < val && currentNode.right == null) {
+                currentNode.right = newNode;
+                break;
+            }
+        }
+        return root;
+    }
+}
