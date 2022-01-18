a4d88d782e751c36cacec956aa292433ceb90bd4
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2022-01-18 18:45:09 +0530

    teemo Attacking solved

--- .dev/allProblemsSolved.py
+++ .dev/allProblemsSolved.py
@@ -19,31 +19,25 @@ def difficulty (line):
         return '<img src="https://img.shields.io/badge/-Hard-red">'
     else :
         return line
-    
 
 for number in range(len(lines)):
     i= lines[number]
     p=i.index("```")
     writeReadme.write(str(number)+". ["+ i[:i.rindex("/")] +"]("+i[:p]+") "+  difficulty(i[p:])+"\n")
 
-
 # #Additional Problems Here 
 writeReadme.write(str(number)+'. [https://www.codingninjas.com/reverse-first-k-elements-of-queue](https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118523/offering/1380947?leftPanelTab=0) '+difficulty('easy')+'\n') 
-
-
 
 easy = "<img src='https://img.shields.io/badge/"+str(array[0])+" -Easy Problems Solved-brightgreen '> " 
 medium = "<img src='https://img.shields.io/badge/"+str(array[1])+" -Medium Problems Solved-blue '> "
 hard = "<img src='https://img.shields.io/badge/"+str(array[2])+" -Hard Problems Solved-red '> "
-
-
 
 writeReadme.write( "\n### Yeah ofc we'd start counting from 0 duh !!\n")
 writeReadme.write(easy)
 writeReadme.write(medium)
 writeReadme.write(hard)
 
-f.close()
 print(array, sum(array))
+
 f.close()
 writeReadme.close()
