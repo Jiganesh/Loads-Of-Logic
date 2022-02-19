3b69c074bda49ab2cd59db8969bbb306963e51d6
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2022-02-19 19:42:49 +0530

    all elements in tow binary search solved

---
+++ trees/TreeNode.java
@@ -0,0 +1,16 @@
+package trees;
+
+
+//Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
