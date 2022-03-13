58c23ccda08ef0ee48ad14fafab57ec1711e0f27
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2022-03-13 11:22:40 +0530

    valid parentheses daily coding challenge problem solved

--- stack/validParentheses.py
+++ stack/validParentheses.py
@@ -1,19 +1,19 @@
-#https://leetcode.com/problems/valid-parentheses/
+# https://leetcode.com/problems/valid-parentheses/
 
 # Runtime: 24 ms, faster than 96.14% of Python3 online submissions for Valid Parentheses.
 # Memory Usage: 14.4 MB, less than 36.61% of Python3 online submissions for Valid Parentheses.
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        from collections import deque
-        mapp={'}':'{', ')':'(', ']':'['}
-        stack=deque()
-        for i in s:
-            if i in mapp.values():
-                stack.append(i)
-            elif stack and mapp[i]==stack[-1]:
-                stack.pop()
-            else:
-                return False
         
+        dic = {"}":"{", "]":"[", ")":"("}
+        
+        stack = []
+        
+        for i in s:
+            if stack and i in dic and stack[-1] == dic[i]:
+                stack.pop()
+            else :
+                stack.append(i)
+                
         return len(stack)==0
