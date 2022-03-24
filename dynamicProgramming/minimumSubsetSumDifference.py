adf9620420d61af9ebe6a04acf2ba6f8eee14639
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2022-03-24 17:19:16 +0530

    solved minimum subset difference dynaimic programming problem

---
+++ dynamicProgramming/minimumSubsetSumDifference.py
@@ -0,0 +1,54 @@
+# Question : https://www.youtube.com/watch?v=-GtpxG6l_Mc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=10&ab_channel=AdityaVerma
+
+def minimumSubsetSumDifference (arr):
+
+    maxEleFromSubset = sum (arr)
+
+    dp = [ [0]*(maxEleFromSubset+1) for i in range (len(arr)+1)]
+
+
+    # Finding which numbers are possible from subset
+    for i in range (len(arr)+1):
+        for j in range (maxEleFromSubset+1):
+            if i == 0 :
+                dp [i][j]= False
+            if j ==0:
+                dp[i][j]= True
+
+    for i in range (1, len(arr)+1):
+        for j in range (maxEleFromSubset+1):
+            if arr[i-1]<= j :
+                dp[i][j]=  dp[i-1][j] or dp [i-1][j - arr[i-1]]
+            else :
+                dp[i][j]=  dp[i-1][j]
+
+
+    # Uncomment to view dp matrix
+    # for i in dp :
+    #     for j in i:
+    #         print(j ,end ="  ")
+    #     print()
+
+
+    # Possible Numbers from subset
+    numbersPossibleFromSubset = []
+    for i in range (maxEleFromSubset+1):
+
+        if dp[len(arr)][i] :
+            numbersPossibleFromSubset.append(i)
+
+    print( numbersPossibleFromSubset)
+
+
+    # Ofc the minimum difference will be present in middle
+    mid = len(numbersPossibleFromSubset)//2
+    s1 = numbersPossibleFromSubset[mid]
+    s2 = maxEleFromSubset-s1
+    
+
+    return abs(s2-s1)
+
+print(minimumSubsetSumDifference([1,2,7])) # 7 , 2+1
+print(minimumSubsetSumDifference([2,3,7,8,10])) # 7+8 , 10+2+3
+print(minimumSubsetSumDifference([7,7,7,7,7,7])) # 7+7+7 , 7+7+7
+print(minimumSubsetSumDifference([1, 5, 6, 98, 84]))
