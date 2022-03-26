aab7b6d88e2e790bc91ae7107b2d98cd6509cc61
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-03-27 00:15:17 +0530

    Created removeDuplicatesFromSortedArray.py
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ arrays/removeDuplicatesFromSortedArray.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+# Runtime: 138 ms, faster than 47.61% of Python3 online submissions for Remove Duplicates from Sorted Array.
+# Memory Usage: 15.6 MB, less than 21.60% of Python3 online submissions for Remove Duplicates from Sorted Array.
+
+
+def removeDuplicates(self, nums: list[int]) -> int:
+    k = 0
+    for i in range(len(nums)):
+        if i < len(nums) - 1 and nums[i] == nums[i + 1]:
+            continue
+        nums[k] = nums[i]
+        k += 1
+    return k
