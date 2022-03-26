f3ec37585471ce1fe9873637ef3d503b4962f32d
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2022-03-27 00:22:33 +0530

    added class solution

--- arrays/removeDuplicatesFromSortedArray.py
+++ arrays/removeDuplicatesFromSortedArray.py
@@ -4,11 +4,12 @@
 # Memory Usage: 15.6 MB, less than 21.60% of Python3 online submissions for Remove Duplicates from Sorted Array.
 
 
-def removeDuplicates(self, nums: list[int]) -> int:
-    k = 0
-    for i in range(len(nums)):
-        if i < len(nums) - 1 and nums[i] == nums[i + 1]:
-            continue
-        nums[k] = nums[i]
-        k += 1
-    return k
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                continue
+            nums[k] = nums[i]
+            k += 1
+        return k
