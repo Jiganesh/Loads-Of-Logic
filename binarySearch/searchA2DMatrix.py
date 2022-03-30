5d0f857edab4813d2614181cc90dd48296df47e9
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2022-03-30 12:29:02 +0530

    wrong variable name fixed

--- binarySearch/searchA2DMatrix.py
+++ binarySearch/searchA2DMatrix.py
@@ -73,7 +73,7 @@ class Solution:
             elif matrix[pointerRow][pointerCol]<target:
                 start=mid+1
             else:
-                r=mid-1
+                end=mid-1
         
         return False
         
