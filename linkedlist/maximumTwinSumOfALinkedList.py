a75e206f245555b547fc4fca70fd7b9be0fcdf2d
pyyhacker <jiganeshpatil01071999@gmail.com>
2022-08-24 02:58:55 +0530

    linked list problem

--- linkedlist/maximumTwinSumOfALinkedList.py
+++ linkedlist/maximumTwinSumOfALinkedList.py
@@ -1,9 +1,14 @@
+# https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution(object):
+    # TC: O(N)
+    # SC: O(N)
     def pairSum(self, head):
         """
         :type head: Optional[ListNode]
@@ -25,4 +30,47 @@ class Solution(object):
             maximum = max(maximum, sum)
             i+=1;
         return maximum
+
+    # TC : O(N)
+    # SC : O(1)
     
+    # Runtime: 1653 ms, faster than 25.95% of Python3 online submissions for Maximum Twin Sum of a Linked List.
+    # Memory Usage: 45.1 MB, less than 71.22% of Python3 online submissions for Maximum Twin Sum of a Linked List.
+    def pairSum(self, head):
+        
+        # Step 1
+        # Find the middle node
+        
+        slow = fast = first_head = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Step 2
+        # Reverse the second half
+        second_head = slow
+        prev = None
+        
+        while second_head:
+            nxt = second_head.next
+            second_head.next = prev
+            
+            prev = second_head
+            second_head = nxt
+            
+        second_head = prev
+        
+        # Step 3
+        # Traverse through both ends
+        maximum_twin_sum = float("-inf")
+        while first_head and second_head:
+
+            maximum_twin_sum = max(maximum_twin_sum, first_head.val + second_head.val)
+            first_head = first_head.next
+            second_head = second_head.next
+            
+        return maximum_twin_sum
+        
+            
+        
