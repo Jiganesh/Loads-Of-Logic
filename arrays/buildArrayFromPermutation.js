75d6bd01f50a3455e8d792e9de347c6f4b42115e
Udoka <udochukwukaonyela@gmail.com>
2022-10-04 21:33:06 +0100

    added new run time methods

--- arrays/buildArrayFromPermutation.js
+++ arrays/buildArrayFromPermutation.js
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/build-array-from-permutation/
+// Runtime: 248 ms, faster than 5.09% of JavaScript online submissions for Build Array from Permutation.
+// Memory Usage: 48.1 MB, less than 6.00% of JavaScript online submissions for Build Array from Permutation.
+
 /**
  * @param {number[]} nums
  * @return {number[]}
