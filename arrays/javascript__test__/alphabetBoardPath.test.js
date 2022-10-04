008dda80406d948fafc7475eaba90d736c29f547
Udoka <udochukwukaonyela@gmail.com>
2022-10-04 20:59:56 +0100

    created alphabet Board path

---
+++ arrays/javascript__test__/alphabetBoardPath.test.js
@@ -0,0 +1,30 @@
+const Test = {
+  assertSimilar: require("assert").deepStrictEqual,
+  assertEquals: require("assert").strictEqual,
+};
+
+const alphabetBoardPath = require("../alphabetBoardPath");
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(
+      alphabetBoardPath("leet"),
+      "DDR!UURRR!!DDD!",
+      "Expected to be DDR!UURRR!!DDD!"
+    );
+    Test.assertEquals(
+      alphabetBoardPath("code"),
+      "RR!DDRR!UUL!R!",
+      "Expected  to be RR!DDRR!UUL!R!"
+    );
+    Test.assertEquals(
+      alphabetBoardPath("leetcodez"),
+      "DDR!UURRR!!DDD!UUULL!DDRR!UUL!R!DDDDLLLLD!",
+      "Expected  to be DDR!UURRR!!DDD!UUULL!DDRR!UUL!R!DDDDLLLLD!"
+    );
+    Test.assertEquals(
+      alphabetBoardPath("zdz"),
+      "DDDDD!UUUUURRR!DDDDLLLD!",
+      "Expected  to be DDDDD!UUUUURRR!DDDDLLLD!"
+    );
+  });
+});
