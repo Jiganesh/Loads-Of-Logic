2467c939cb1e70b5964000df6e76deeca2c8dd5f
Udoka <udochukwukaonyela@gmail.com>
2022-10-04 15:29:16 +0100

    created alphabet board path

---
+++ arrays/alphabetBoardPath.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} target
+ * @return {string}
+ */
+var alphabetBoardPath = function (target) {
+  const board = ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"];
+  const targetArr = target.split("");
+  targetArr.forEach((target, i) => {
+    board.forEach((value, index) => {
+      const valueArr = value.split("");
+        valueArr.forEach((str,i) => {
+          
+      });
+    });
+  });
+};
