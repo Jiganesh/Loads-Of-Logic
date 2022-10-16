e328f0f48db4595a249acd4937ceefa9c66c71a1
pyyhacker <jiganeshpatil01071999@gmail.com>
2022-10-16 12:32:24 +0530

    contest problem

---
+++ arrays/largestPositiveIntegerThatExistsWithItsNegative.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/
+
+from typing import List
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        
+        lookup = set(nums)
+
+        result = float("-inf")
+        for num in nums:
+            if -num in lookup:
+                result = max(result, num)
+
+        return result if result != float("-inf") else -1
+ 
