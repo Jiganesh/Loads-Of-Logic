b0beb7e1f78bdba55a7cc3f73d099fc84a9a3960
Jiganesh Patil <jiganeshpatil01071999@gmail.com>
2023-04-27 00:33:08 +0530

    daily challenge

--- maths/addDigits.py
+++ maths/addDigits.py
@@ -15,19 +15,17 @@ class Solution(object):
             num = temp
         return num
 
-    def addDigits(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
-        
-        while num > 9:
-            temp = 0
-            while num>0:
-                temp+=num%10
-                num = num//10
-            num = temp
-        return num
-        
+    # TC : O(1)
+    # SC : O(1)
+    def addDigits(self, num: int) -> int:
+
+        if num == 0 :
+            return 0
+        elif num %  9 == 0:
+            return 9
+        else :
+            return num % 9
+
+         
         
 print(Solution().addDigits(38))
